8e03f67f48301d28100a2c2cf5e2a6aa5cf8df83
s1mpleNova <shahnaman632002@gmail.com>
2024-03-10 11:59:21 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,8 +8,13 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text(
+            'Dicee',
+            style: TextStyle(
+              color: Colors.white, // Custom text color
+            ),
+          ),
+          backgroundColor: Color.fromRGBO(32, 22, 88, 3),
         ),
         body: DicePage(),
       ),
@@ -15,18 +22,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldn = 1;
+  int rdn = 1;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('iamges/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('iamges/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      ldn = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$ldn.png')),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      rdn = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$rdn.png')),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
